ef479f636fd1b379c4713af94048b45c2b5736c6
Valdir Santos <valdirsantost40@gmail.com>
2024-04-05 19:16:57 -0300

    Criada funções para calcular o imc

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,7 +1,35 @@
 import { Button, Text, TextInput, View } from "react-native";
-
+import React, { useState } from "react";
 
 export default function Form() {
+    
+    const [height, setHeight] = useState(null)
+    const [weight, setWeight] = useState(null)
+    const [textButton, setTextButton] = useState("Calcular")
+    const [imc, setImc] = useState(null)
+    const [mensagemImc, setMensagemImc] = useState("Preencha os campos acima")
+    
+
+    function imcCalculator() {
+        return setImc((weight / (height * height))).toFixed(2); //Calcula o imc
+        //O metódo tofixed só amostra as casas decimais do resultado da divisão de acordo com o número que vc definir 
+    }
+
+    function validatorImc() {
+        //Esta função verifica se os campos de altura e peso estão preenchidos ou não, se estiverem e vc tiver apertado o botão de calcular, irá sumir o que vc escreveu, vai mudar texto do botão e amostrar o resultado do cálculo
+        if (weight != null && height != null) {
+            imcCalculator()
+            setHeight(null)
+            setWeight(null)
+            setTextButton("Calcular novamente")
+            setMensagemImc("Seu imc é igual a:")
+            return
+
+        } else {
+            setImc(null)
+            setTextButton("Calcular") //Se estiverem nulos o imc terá valor de null e o botão continuará com o nome calcular
+        }
+    }
     return (
         <View>
             <View>
@@ -18,7 +46,7 @@ export default function Form() {
                 ></TextInput>
 
                 <Button
-                title="Calcular Imc"
+                title={textButton}
                 ></Button>
             </View>
 
